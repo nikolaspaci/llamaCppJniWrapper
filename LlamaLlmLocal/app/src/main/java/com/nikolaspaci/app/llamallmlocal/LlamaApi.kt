aa4126f722a85539d694a1d187407aa5033db426
nikolaspaci <nikolaspaci@gmail.com>
2025-08-10 22:16:41 +0200

    generify optims for librairy

--- LlamaLlmLocal/app/src/main/java/com/nikolaspaci/app/llamallmlocal/LlamaApi.kt
+++ LlamaLlmLocal/app/src/main/java/com/nikolaspaci/app/llamallmlocal/LlamaApi.kt
@@ -1,5 +1,7 @@
 package com.nikolaspaci.app.llamallmlocal
 
+import android.os.Build
+
 interface PredictCallback {
     fun onToken(token: String)
     fun onComplete(tokensPerSecond: Double, durationInSeconds: Long)
@@ -7,7 +9,22 @@ interface PredictCallback {
 }
 object LlamaApi {
     init {
-        System.loadLibrary("jniLlamaCppWrapper")
+        when (Build.SUPPORTED_ABIS.firstOrNull()) {
+            "arm64-v8a" -> {
+                try {
+                    // Attempt to load the most optimized library first.
+                    System.loadLibrary("jniLlamaCppWrapper_armv9-a")
+                } catch (e: UnsatisfiedLinkError) {
+                    // If it fails, the CPU likely doesn't support v9 instructions.
+                    // Fall back to the more compatible v8.2-a library.
+                    System.loadLibrary("jniLlamaCppWrapper_v82a")
+                }
+            }
+            else -> {
+                // For other architectures (x86_64, etc.), load the generic library.
+                System.loadLibrary("jniLlamaCppWrapper")
+            }
+        }
     }
 
     external fun init(modelPath: String): Long
